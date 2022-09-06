e09085136ec6d65f74772fbe5985cef7495e7a43
Ivan <belogurov.ivan@list.ru>
2022-09-06 10:15:45 +0500

    finished the home page

--- main/views.py
+++ main/views.py
@@ -1,9 +1,20 @@
+from django.contrib.auth.models import User
 from django.shortcuts import render
 from django.views.generic import TemplateView
+
+from articles.models import Article
 
 
 class ShowHomePage(TemplateView):
     template_name = 'main/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        context['articles_amount'] = Article.objects.filter(is_published=True).count
+        context['users_amount'] = User.objects.count
+
+        return context
 
 
 def page_not_found(request, exception):
